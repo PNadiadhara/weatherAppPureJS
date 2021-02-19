6927ca04e926baf0c0fb608995feb87e252b22a5
PNadiadhara <43886212+PNadiadhara@users.noreply.github.com>
2021-02-18 22:25:23 -0500

    test

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const https = require("https");
 const app = express();
 
 app.get("/", function(req,res){
-    const url = "https://api.openweathermap.org/data/2.5/weather?q=new york&appid=dc98595592480b61420cbef0fb3d02b4"
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=new york&appid=dc98595592480b61420cbef0fb3d02b4&units=imperial"
     https.get(url, function(response){
         console.log(response.statusCode);
         // "data" keyword found from nodejs built in https require doc
@@ -12,9 +12,12 @@ app.get("/", function(req,res){
             console.log(data);
             const weatherData = JSON.parse(data)
             console.log(weatherData)
+            const temp = weatherData.main.temp
+            console.log(temp);
         })
     })
     res.send("Server is Up and running")
+    res.send("Test")
 })
 
 
